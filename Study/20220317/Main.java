fffa18ca3c3d190b6db962aafd0d5ecaaca5bb84
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-18 00:35:20 +0900

    2022년03월17일
    스터디 예제

---
+++ Study/20220317/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("이름 : 유승준");
+        System.out.println("성별 : 남");
+        System.out.println("이메일 : teddy154@naver.com");
+
+    }
+
+}
