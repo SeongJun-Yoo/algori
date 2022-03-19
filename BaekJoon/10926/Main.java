887ca27970cea104567b0d85354d174334768405
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-19 13:55:38 +0900

    백준 10926 문제
    입력값에 !!? 추가해서 출력

---
+++ BaekJoon/10926/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String ID = scan.nextLine();
+
+        System.out.print(ID + "!!?");
+
+    }
+
+}
