ca0a6956ba874c020df0758007a4d70b3ca1b0e1
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-21 22:41:23 +0900

    20220319
    코딩 정리

---
+++ Study/20220319/MyProfile.java
@@ -0,0 +1,13 @@
+public class MyProfile {
+    public static void main(String[] args){
+        /*  println -> print 수정
+        System.out.println("유승준");
+        System.out.println("teddy154@naver.com");
+        System.out.println("남자");
+        */
+
+        System.out.print("유승준");
+        System.out.print("teddy154@naver.com");
+        System.out.print("남자");
+    }
+}
