28e39b3d655943d6758d9df2b8debd4303c2d424
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-21 22:37:08 +0900

    백준 2741 문제
    1 ~ N 출력

---
+++ BaekJoon/2741/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String count = reader.readLine();
+            int num = Integer.parseInt(count);
+            for (int i = 1; i <= num; i++) {
+                bw.write(Integer.toString(i));
+                bw.newLine();
+            }
+
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
