cba59be8b27aacdebd326560622c10499eadb9b8
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-24 23:56:41 +0900

    백준 1110 문제
    수정

--- BaekJoon/1110/Main.java
+++ BaekJoon/1110/Main.java
@@ -5,33 +5,23 @@ public class Main {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            String num = reader.readLine();
-            int total = Integer.parseInt(num);
-            int first = Integer.parseInt(num);
-            total = 26;
+  
+            int num = Integer.parseInt(reader.readLine());
+            int total = num;
             int count = 0;
-            boolean a = true;
+         
             int num1 = 0;
             int num2 = 0;
-
+            boolean a = true;
             while (a) {
-                num1 = total / 10;
-                num2 = total % 10;
-                if (num1 + num2 < 10) {
-                    total = (num1 + num2) + num2;
-                } else if((num1+num2)>=10){
-                    total = ((num1 + num2) % 10) + num2;
-                }
-                count += 1;
-                if (total == first) {
+               total = ((total % 10)*10) + (((total / 10)+(total %10)) % 10);
+                count++;
+                if(total == num){
                     a = false;
                 }
+               
             }
-            bw.write(Integer.toString(count));
-            bw.newLine();
-            bw.flush();
-            bw.close();
+            System.out.print(count);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
