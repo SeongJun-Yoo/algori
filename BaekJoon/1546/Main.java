0e4689aac42002ec522a0ee0b665c755faa48007
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-03-31 00:27:02 +0900

    백준 1546 문제
    최댓값 평균

--- BaekJoon/1546/Main.java
+++ BaekJoon/1546/Main.java
@@ -5,27 +5,25 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        int cnt = scan.nextInt();
-        int[] num = new int[cnt];
-        int M = 0;
-        int total = 0;
+        double[] num = new double[scan.nextInt()];
+        double M = 0;
+        double total = 0;
         double avg = 0.0;
-        for (int i = 0; i<cnt;i++){
+        for (int i = 0; i < num.length; i++) {
             num[i] = scan.nextInt();
-      
-            if (num[i]>M){
+            if (num[i] > M) {
                 M = num[i];
-         
+
             }
         }
-    
-        for (int i = 0; i < cnt ; i++){
-            num[i] =  (num[i] *100)/M ;
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (num[i] * 100) / M;
             total = total + num[i];
-           
+
         }
-        avg = total/cnt;
-       System.out.println(avg);
+        avg = total / (double) num.length;
+        System.out.println(avg);
 
     }
 
