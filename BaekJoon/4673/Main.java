d01939f2604479cde92e10314f429981fd52ef85
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-04-03 22:50:22 +0900

    백준 4673 문제
    char to int

---
+++ BaekJoon/4673/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+     
+        System.out.println(aski(str.charAt(0)));
+        
+    }
+
+    static int aski(char ch) {
+        int num = ch ;
+       
+        return num;
+    }
+}
