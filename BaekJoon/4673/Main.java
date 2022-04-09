beeb410f8846062fbb2fe50ec17d2a6cafda35f2
SeongJun-Yoo <79687290+SeongJun-Yoo@users.noreply.github.com>
2022-04-09 21:38:28 +0900

    백준 4673 문제
    셀프 넘버

--- BaekJoon/4673/Main.java
+++ BaekJoon/4673/Main.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        String str = scan.nextLine();
-     
-        System.out.println(aski(str.charAt(0)));
-        
+        for (int i = 1; i < 10001; i++) {
+            if (self(i) == true) {
+                System.out.println(i);
+            }
+        }
     }
 
-    static int aski(char ch) {
-        int num = ch ;
-       
-        return num;
+    public static boolean self(int a) {
+
+        for (int i = 1; i < a; i++) {
+            int k = a - i;
+            int m = a - i;
+            int j = 0;
+            while (k > 0) {
+                j += k % 10;
+                k = k / 10;
+            }
+            if (j != 0 && a == m + j)
+                return false;
+        }
+        return true;
     }
 }
